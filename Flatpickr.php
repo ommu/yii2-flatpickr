9837118339b09cf5fdbe0f4aa1ace264d77b59a7
Putra Sudaryanto <putra@ommu.id>
2025-01-22 09:37:32 +0700

    update contact

--- Flatpickr.php
+++ Flatpickr.php
@@ -4,7 +4,7 @@
  * @package ommu\flatpickr
  * 
  * @author Putra Sudaryanto <putra@ommu.id>
- * @contact (+62)856-299-4114
+ * @contact (+62)811-2540-432
  * @copyright Copyright (c) 2020 OMMU (www.ommu.id)
  * @created date 11 August 2020, 14:29 WIB
  * @link https://github.com/ommu/yii2-flatpickr
